2cf6fabbdb14fddb1d986c8e7be45c7ef4c3e183
chaewonkim <kimlove9182@gmail.com>
2024-04-04 19:23:26 +0900

    chore: 주석 추가

--- SpringJWT/src/main/java/com/example/SpringJWT/config/CorsMvcConfig.java
+++ SpringJWT/src/main/java/com/example/SpringJWT/config/CorsMvcConfig.java
@@ -3,6 +3,7 @@ package com.example.SpringJWT.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//다른 controller단 문제도 처리해주기 위해서
 public class CorsMvcConfig implements WebMvcConfigurer {
 
     @Override
