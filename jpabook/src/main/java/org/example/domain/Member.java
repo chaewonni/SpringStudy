5c5b7fc63ab09f4bf15d8b4bd50b7b14713be87f
chaewonkim <kimlove9182@gmail.com>
2024-05-24 16:36:35 +0900

    feat: 공부 code 업로드

--- jpabook/src/main/java/org/example/domain/Member.java
+++ jpabook/src/main/java/org/example/domain/Member.java
@@ -52,4 +52,14 @@ public class Member extends BaseEntity {
     public void setOrders(List<Order> orders) {
         this.orders = orders;
     }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "orders=" + orders +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
 }
